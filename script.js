b467db5bbe2fcd9824a31aff011db54f4e536ed3
DelrioRamirez4321 <delrioramirez7813@gmail.com>
2020-03-11 11:47:46 -0700

    did all the questions

--- script.js
+++ script.js
@@ -4,19 +4,19 @@
 console.log(
   "What worked well for you over the last 9 weeks that aided in your learning process?"
 );
-console.log("");
+console.log("over the last nine weeks the thing that aid me the most in learning a lot of the javascript was your demos and the recourse page.   ");
 
 // 2. Write a console.log() statement that answers the following question: What would you
 // change about the course?
 
 console.log("What would you change about the course?");
-console.log("");
+console.log("If there is one thing that should change about the class is use of the book.  I felt the book was really vague about some part and were really hard to understand.   ");
 
 // 3. Write a console.log() statement that answers the following question: Did you learn
 // what you expected to learn in this course?
 
 console.log("Did you learn what you expected to learn in this course?");
-console.log("");
+console.log("Yeah");
 
 // 4. Write a console.log() statement that answers the following question: What concepts
 // were new to you and what concepts had you encountered already in the past?
@@ -24,4 +24,4 @@ console.log("");
 console.log(
   "What concepts were new to you and what concepts had you encountered already in the past?"
 );
-console.log("");
+console.log("everything that I have learn in the class was completely new to me because I had no programming knowledge I only new html and css.  ");
